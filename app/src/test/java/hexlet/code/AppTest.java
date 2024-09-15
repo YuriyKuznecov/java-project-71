61f8422345fb749ace88e318aa437e9d59ba000d
Yuriy Kuznecov <ura141982@gmail.com>
2024-09-15 15:03:25 +0300

    fix project

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -36,28 +36,28 @@ class AppTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {".json", ".yaml"})
+    @ValueSource(strings = {".json", ".yml"})
     public void testStylishFormat(String s) throws IOException {
         var actual = Differ.generate(FILE_1 + s, FILE_2 + s, "stylish");
         assertEquals(stylish, actual);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {".json", ".yaml"})
+    @ValueSource(strings = {".json", ".yml"})
     public void testPlainFormat(String s) throws IOException {
         var actual = Differ.generate(FILE_1 + s, FILE_2 + s, "plain");
         assertEquals(plain, actual);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {".json", ".yaml"})
+    @ValueSource(strings = {".json", ".yml"})
     public void testJsonFormat(String s) throws IOException {
         var actual = Differ.generate(FILE_1 + s, FILE_2 + s, "json");
         assertEquals(json, actual);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {".json", ".yaml"})
+    @ValueSource(strings = {".json", ".yml"})
     public void testDefaultFormat(String s) throws IOException {
         var actual = Differ.generate(FILE_1 + s, FILE_2 + s);
         assertEquals(stylish, actual);
